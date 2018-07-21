381e593ecaeb8922fdbda8e8e493fb6f83029497
Elaine Wu <elawu@ucdavis.edu>
2018-07-21 01:10:13 -0700

    edit: count down

--- project_python/ch_03/count_down.py
+++ project_python/ch_03/count_down.py
@@ -2,7 +2,8 @@
 Exercise: Count down
 
 Objective: Write a while loop that changes a variable and stops when that variable reaches a particular value.
-Write a program that counts down from 100 to 0 backwards by twos. If you need to, write out all of the print statements by hand, look at what’s changing, and introduce a variable to allow that change using repeated lines of code. Then wrap into a while loop.
+Write a program that counts down from 100 to 0 backwards by twos. If you need to, write out all of the print statements by hand, look at
+what’s changing, and introduce a variable to allow that change using repeated lines of code. Then wrap into a while loop.
 '''
 i = 100
 while (i>=0):
